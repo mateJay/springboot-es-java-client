65a9629ed39b1c97bbb6bad1b4cda4af80cdb1d2
DunShuai Li <lidunshuai@xman.studio>
2020-03-13 09:59:54 +0800

    初始化项目

---
+++ src/main/java/com/matejay/es/EsJavaHighLevelClientApplication.java
@@ -0,0 +1,13 @@
+package com.matejay.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsJavaHighLevelClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsJavaHighLevelClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matejay/es/EsJavaHighLevelClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matejay.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsJavaHighLevelClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
