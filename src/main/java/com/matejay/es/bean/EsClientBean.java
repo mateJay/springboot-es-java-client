acfe4b935e9305fadb2e56cc09cd8cb1cb6d6423
DunShuai Li <lidunshuai@xman.studio>
2020-04-05 12:27:14 +0800

    完善search方法，增加get，index，delete方法

--- src/main/java/com/matejay/es/bean/EsClientBean.java
+++ src/main/java/com/matejay/es/bean/EsClientBean.java
@@ -2,6 +2,12 @@ package com.matejay.es.bean;
 
 import com.matejay.es.constant.PropertyKeyConst;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -9,17 +15,20 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.ScoreSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 
 public class EsClientBean {
     private Properties properties;
     private RestHighLevelClient client;
+    private String index;
     SearchRequest searchRequest;
 
     private Logger logger = LoggerFactory.getLogger(EsSongClient.class);
@@ -39,6 +48,7 @@ public class EsClientBean {
     }
 
     public void setIndex(String index) {
+        this.index = index;
         searchRequest = new SearchRequest(index);
     }
 
@@ -57,11 +67,18 @@ public class EsClientBean {
     }
 
     public SearchResponse defaultSearch(QueryBuilder query, Integer from, Integer size) throws IOException {
+        return defaultSearch(query, from, size, null);
+    }
+
+    public SearchResponse defaultSearch(QueryBuilder query, Integer from, Integer size, HighlightBuilder highlightBuilder) throws IOException {
         SearchSourceBuilder searchSource = new SearchSourceBuilder();
         searchSource.query(query);
         searchSource.from(from);
         searchSource.size(size);
         searchSource.sort(new ScoreSortBuilder().order(SortOrder.DESC));
+        if (highlightBuilder != null) {
+            searchSource.highlighter(highlightBuilder);
+        }
         return search(searchSource);
     }
 
@@ -72,6 +89,21 @@ public class EsClientBean {
     public SearchResponse search(SearchSourceBuilder searchSource, RequestOptions requestOptions) throws IOException {
         searchRequest.source(searchSource);
         return client.search(searchRequest, requestOptions);
+    }
+
+    public GetResponse get(String id) throws IOException {
+        GetRequest getRequest = new GetRequest(index, id);
+        return client.get(getRequest, RequestOptions.DEFAULT);
+    }
+
+    public IndexResponse index(String id, Map<String, ?> source) throws IOException {
+        IndexRequest indexRequest = new IndexRequest(index).id(id).source(source);
+        return client.index(indexRequest, RequestOptions.DEFAULT);
+    }
+
+    public DeleteResponse delete(String id) throws IOException {
+        DeleteRequest deleteRequest = new DeleteRequest(index, id);
+        return client.delete(deleteRequest, RequestOptions.DEFAULT);
     }
 
     public Properties getProperties() {
